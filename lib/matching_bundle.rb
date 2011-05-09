1173cb489c930208b85d2c569fd6b10ef7ae2155
grosser <grosser.michael@gmail.com>
2011-05-09 20:15:04 +0200

    make that executable work

--- lib/matching_bundle.rb
+++ lib/matching_bundle.rb
@@ -3,6 +3,20 @@ require 'open-uri'
 class MatchingBundle
   VERSION = File.read( File.join(File.dirname(__FILE__),'..','VERSION') ).strip
 
+  def self.find_or_install_matching_version(gemfile_content)
+    return unless requirement = bundler_requirement(gemfile_content)
+
+    if version = find_matching_local_bundler_version(requirement)
+      return version
+    end
+
+    if version = find_matching_remote_bundler_version(requirement)
+      $stderr.puts "installing bundler #{version}"
+      `gem install bundler -v #{version}`
+      return version
+    end
+  end
+
   def self.find_matching_remote_bundler_version(requirement)
     json = open('http://rubygems.org/api/v1/versions/bundler').read
     versions = json.scan(/"number"\s*:\s*"(.*?)"/).map{|v|v.first}
